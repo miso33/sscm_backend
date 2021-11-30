3e8a58fd2ef52007d6f4ddec1aedd929212daefe
mgacko <mgacko@gmail.com>
2021-11-30 21:05:41 +0100

    - scrypt for delete users

--- scripts/delete.py
+++ scripts/delete.py
@@ -2,4 +2,17 @@ from rest_framework_simplejwt.token_blacklist.models import OutstandingToken
 
 
 def run():
-    OutstandingToken.objects.filter(user__email='labas@scrypta.sk').delete()
+    OutstandingToken.objects.filter(
+        user__email_in=[
+            "domestro@outlook.com",
+            "ehospodarova @ gmail.com",
+            "hospodar@saske.sk",
+            "info@scrypta.sk",
+            "jozefma1k@scrypta.sk",
+            "mariabodn @ gmail.com",
+            "mark.varchola@gmail.com",
+            "mark.varchola+1@gmail.com",
+            "mgacko@gmail.com",
+            "mkogaci@gmail.com"
+        ]
+    ).delete()
