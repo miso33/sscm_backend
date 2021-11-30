0990be6d8b654dd0bc2d0dc27b39da7a9de0489a
mgacko <mgacko@gmail.com>
2021-11-30 21:10:30 +0100

    - scrypt for delete users

--- scripts/delete.py
+++ scripts/delete.py
@@ -5,11 +5,9 @@ def run():
     OutstandingToken.objects.filter(
         user__email_in=[
             "domestro@outlook.com",
-            "ehospodarova @ gmail.com",
             "hospodar@saske.sk",
             "info@scrypta.sk",
             "jozefma1k@scrypta.sk",
-            "mariabodn @ gmail.com",
             "mark.varchola@gmail.com",
             "mark.varchola+1@gmail.com",
             "mgacko@gmail.com",
